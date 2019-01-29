c34502109eebbbf239d88679a5ac911c4fe25deb
Ethan Roberts <ejrobe@gmail.com>
2019-01-29 22:00:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
 class Dog
-  def name=
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
